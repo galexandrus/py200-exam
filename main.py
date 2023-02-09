12a2548f3f9bb6e0888d8d7fc63f7c4bf2c58bf6
galexandrus <gavrilchuk_a@mail.ru>
2023-02-09 18:59:55 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# TODO В блоке if __name__ == '__main__' проверьте функциональность добавления продуктов в корзину
+#  и отображения корзины пользователя.
+
+from id_counter import IdCounter
+
+
+if __name__ == "__main__":
+    print("let's go!")
